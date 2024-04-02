b03ee9309798e140f226c52caa1189406f6a8de5
guwon <101141876+Kyuwon-Choi@users.noreply.github.com>
2024-04-02 21:20:07 +0900

    [Silver III] Title: STOP USING MONEY, Time: 552 ms, Memory: 131152 KB -BaekjoonHub

--- 백준/Silver/14674. STOP USING MONEY/STOP USING MONEY.py
+++ 백준/Silver/14674. STOP USING MONEY/STOP USING MONEY.py
@@ -1,5 +1,6 @@
 import sys
 from fractions import Fraction
+#파이썬에서 유리수 연산을 정확하게 하려면 fractions.Fraction을 사용해야 한다.
 input = sys.stdin.readline
 
 n,k=map(int, input().split())
@@ -16,6 +17,7 @@ for i in range(n):
 
 for i in range(n-1):
     for j in range(i+1,n):
+        #Fraction
         if Fraction(satisfyList[i],costList[i])<Fraction(satisfyList[j],costList[j]):
             numList[i], numList[j] = numList[j], numList[i]
             satisfyList[i], satisfyList[j] = satisfyList[j], satisfyList[i]
