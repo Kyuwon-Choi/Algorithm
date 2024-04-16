39842424ddd957eaf92f1700a7ee2c9c70837809
guwon <101141876+Kyuwon-Choi@users.noreply.github.com>
2024-04-16 15:42:23 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -0,0 +1,16 @@
+-- 코드를 입력하세요
+SELECT CATEGORY, PRICE as MAX_PRICE, PRODUCT_NAME
+from FOOD_PRODUCT
+where (CATEGORY, PRICE) in (
+    select CATEGORY, max(PRICE)
+    from FOOD_PRODUCT
+    GROUP BY CATEGORY
+)
+and (CATEGORY ='과자' or CATEGORY ='국' or CATEGORY ='김치' or CATEGORY ='식용유')
+order by MAX_PRICE desc
+
+# SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
+# from REST_INFO
+# where (FOOD_TYPE, FAVORITES)  in
+# (select FOOD_TYPE, max(FAVORITES) from REST_INFO group by FOOD_TYPE)
+# order by FOOD_TYPE desc
