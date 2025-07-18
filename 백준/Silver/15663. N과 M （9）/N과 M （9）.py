d2d2fd482b25d0dcb92f8a83453c4c97ae4285de
guwon <101141876+Kyuwon-Choi@users.noreply.github.com>
2025-07-18 16:41:13 +0900

    [Silver II] Title: N과 M (9), Time: 140 ms, Memory: 113212 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,16 +1,27 @@
 import sys
-from collections import deque
-from itertools import permutations
 
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-
 li=list(map(int, input().split()))
+li.sort()
+visit = [False] * n
+ans=[]
 
-ans=list(set(permutations(li,m)))
+def dfs():
+    if len(ans) == m:
+        print(*ans)
+        return
 
-ans.sort()
-for i in ans:
-    print(*i)
+    last = 0
 
+    for i in range(n):
+        if not visit[i] and last != li[i]:
+            visit[i] = True 
+            ans.append(li[i])  
+            last = li[i] 
+            dfs() 
+            ans.pop()
+            visit[i] = False
+
+dfs()
